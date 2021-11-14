ea77b646567ee9e8982f12c629fb2881074dd995
先規　勇樹 <yukisaki80@gmail.com>
2021-11-14 21:53:07 +0900

    一覧画面等を作成

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,11 @@
 module ApplicationHelper
+  def full_title(page_title = '')
+    base_title = 'ママの部屋'
+    if page_title.blank?
+      base_title
+    else
+      "#{page_title} - #{base_title}"
+    end
+
+  end
 end
